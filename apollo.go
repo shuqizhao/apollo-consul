c188ab91dd3d1619fa9616ca7e1f8f22ec013c71
shuqizhao <shuqizhao@bdxdoctor.com>
2019-11-27 12:50:17 +0800

    no consul

--- apollo.go
+++ apollo.go
@@ -5,30 +5,31 @@ import (
 	"github.com/shuqizhao/xcfg"
 )
 
-const CfgName  ="MyApollo"
+const CfgName = "MyApollo"
+
 type Apollo struct {
-	XMLName      xml.Name      `xml:"MyApollo"`
-	ConsulUrl      string `xml:"ConsulUrl,attr"`
-	BuildPath string `xml:"BuildPath,attr"`
-	AfterBuild string `xml:"AfterBuild,attr"`
-	FixPage string `xml:"FixPage,attr"`
-	ServiceGroups      []ServiceGroup `xml:"Services"`
-	MajorVersion int           `xml:"majorVersion,attr"`
-	MinorVersion int           `xml:"minorVersion,attr"`
+	XMLName       xml.Name       `xml:"MyApollo"`
+	ConsulUrl     string         `xml:"ConsulUrl,attr"`
+	BuildPath     string         `xml:"BuildPath,attr"`
+	AfterBuild    string         `xml:"AfterBuild,attr"`
+	FixPage       string         `xml:"FixPage,attr"`
+	ServiceGroups []ServiceGroup `xml:"Services"`
+	MajorVersion  int            `xml:"majorVersion,attr"`
+	MinorVersion  int            `xml:"minorVersion,attr"`
 }
 type ServiceGroup struct {
-	Name string `xml:"Name,attr"`
-	Online bool `xml:"Online,attr"`
-	IsEnable bool `xml:"IsEnable,attr"`
+	Name     string        `xml:"Name,attr"`
+	Online   bool          `xml:"Online,attr"`
+	IsEnable bool          `xml:"IsEnable,attr"`
 	Services []ServiceItem `xml:"Service"`
 }
 type ServiceItem struct {
-	Id string `xml:"Id,attr"`
+	Id      string `xml:"Id,attr"`
 	Address string `xml:"Address,attr"`
-	Url string `xml:"Url,attr"`
-	Port string `xml:"Port,attr"`
-	Tag string `xml:"Tag,attr"`
-	Online bool `xml:"Online,attr"`
+	Url     string `xml:"Url,attr"`
+	Port    string `xml:"Port,attr"`
+	Tag     string `xml:"Tag,attr"`
+	Online  bool   `xml:"Online,attr"`
 }
 
 func NewApollo() Apollo {
